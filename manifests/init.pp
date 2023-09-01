e4226b62bddfa6b6670794675879e7016d09a54f
John Ajera <j.ajera@gns.cri.nz>
2023-09-01 14:50:58 +1200

    initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,12 @@
 notify { 'Hello, World!':
   message => 'Hello, World!',
 }
+
+datadog_agent::integration { 'dns_check':
+  instances => [
+    {
+      hostname   => 'example.com',
+      nameserver => '8.8.8.8',
+    },
+  ],
+}
