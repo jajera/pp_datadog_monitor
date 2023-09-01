c3d666ea87b1fdde7a346eaca956b8f4b86d0596
John Ajera <j.ajera@gns.cri.nz>
2023-09-01 14:57:38 +1200

    initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,13 +2,19 @@ notify { 'Hello, World!':
   message => 'Hello, World!',
 }
 
-include datadog_agent
-
-datadog_agent::integration { 'dns_check':
-  instances => [
-    {
-      hostname   => 'example.com',
-      nameserver => '8.8.8.8',
-    },
-  ],
+# Install the 'datadog-datadog_agent' module
+exec { 'install_datadog-datadog_agent':
+  command => '/opt/puppetlabs/bin/puppet module install datadog-datadog_agent',
+  unless  => '/opt/puppetlabs/bin/puppet module list | grep datadog-datadog_agent',
 }
+
+# # include datadog_agent
+
+# datadog_agent::integration { 'dns_check':
+#   instances => [
+#     {
+#       hostname   => 'example.com',
+#       nameserver => '8.8.8.8',
+#     },
+#   ],
+# }
